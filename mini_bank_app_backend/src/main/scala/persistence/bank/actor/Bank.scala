4067aeffa78a2e0efdea3fe1c9ce2893bda09dcd
quantum-adrenaline-1 <yogiraj.chitam@gmail.com>
2023-09-13 23:15:49 +0530

    Event addition for actor Bank
    Added event 'BankAccountCreated' for Bank actor to track the creation of individual bank account (persistent bank account actor) in companion object 'Bank' for Bank.scala actor class

--- mini_bank_app_backend/src/main/scala/persistence/bank/actor/Bank.scala
+++ mini_bank_app_backend/src/main/scala/persistence/bank/actor/Bank.scala
@@ -2,4 +2,17 @@ package com.fun.mini_bank
 package persistence.bank.actor
 
 class Bank {
+
+}
+
+object Bank {
+  sealed trait Event
+
+  /**
+   * purpose: Event for the Bank actor to keep track of accounts creation in the bank
+   *
+   * @param id : String -> uuid/id of the newly created bank account
+   */
+  case class BankAccountCreated(id: String) extends Event
+
 }
