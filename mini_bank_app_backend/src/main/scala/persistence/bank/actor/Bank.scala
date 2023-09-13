887b74e0da9b25470bd60ff92e11292cbd598885
quantum-adrenaline-1 <yogiraj.chitam@gmail.com>
2023-09-13 23:21:15 +0530

    state addition for Bank actor
    Added state for Bank actor class in Bank.scala which (as a data-structure) stores the created bank account (persistent actors) where key is the unique 'id' of the bank account and value is the reference to the persistent actor of the respective bank account

--- mini_bank_app_backend/src/main/scala/persistence/bank/actor/Bank.scala
+++ mini_bank_app_backend/src/main/scala/persistence/bank/actor/Bank.scala
@@ -1,11 +1,23 @@
 package com.fun.mini_bank
 package persistence.bank.actor
 
+import akka.actor.typed.ActorRef
+
+import persistence.bank_account.actor.PersistentBankAccount.{Command, CreateBankAccount, UpdateBalance, GetBankAccount}
+
 class Bank {
+
+  /**
+   * purpose: store the map of [(key: value) -> (id: actor_reference)] as the state of the Bank actor in order to communicate with the PersistentBankAccount reference when a command is executed on the 'id' of that actor. Using this 'accounts' map, Bank actor will identify the reference of the 'id' received as argument and execute the command associated with the 'id' on that actor reference
+   *
+   * @param accounts : Map[String, ActorRef[Command]] -> map of bank accounts where key is the bank account 'id' and value is the reference of the actor instance of that bank account
+   */
+  case class State(accounts: Map[String, ActorRef[Command]])
 
 }
 
 object Bank {
+
   sealed trait Event
 
   /**
